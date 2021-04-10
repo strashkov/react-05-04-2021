4f2a36d69261ec3267db584497d2a471a549176a
rumnast <rumnast@mail.ru>
2021-04-10 15:22:22 +0300

    lesson2 Homework2

---
+++ students/Rumyantseva/src/components/Message.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Message extends React.Component {
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.text} ({this.props.author})
+            </div>)
+    }
+}
