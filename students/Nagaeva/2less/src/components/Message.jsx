5d2e38d2dbe47115ea56a4fbc472b1bc89815556
nagaeva <[mnomontal@gmail.com]>
2021-04-13 15:23:48 +0300

    2lesson-end

--- students/Nagaeva/2less/src/components/Message.jsx
+++ students/Nagaeva/2less/src/components/Message.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 export default class Message extends React.Component {
     static propTypes = {
         text: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired
     };
 
     render() {
