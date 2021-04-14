3bf3e3240be3f717ba779a9b62545d468641df68
selyana <olesya.krizhanovskaya@yandex.ru>
2021-04-14 23:36:27 +0300

    added lesson3

---
+++ students/KrizhanovskaiaLesia/lesson3/static_src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          paddingBottom: 30,
+          textAlign: "center",
+        }}
+      >
+        Это хэдер
+      </div>
+    );
+  }
+}
