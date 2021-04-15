79f187e32e166244c1750377c0c0401b838cad64
Vilen <vilen.bejenaru@wakatech.ro>
2021-04-15 18:42:43 +0300

    added homework 2

--- students/bezhenaru/src/index.jsx
+++ students/bezhenaru/src/index.jsx
@@ -1,45 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './components/MessageField.jsx';
 
-const messages = [
-    'GB',
-    'Hi!',
-    'Hello'
-];
-
-const Message = ({ message }) => {
-    return (
-        <div className="message">{message}</div>
-    );
-};
-
-const MessageList = (props) => {
-    return props.messages.map((message, index) => {
-        return <Message key={index} message={message} />
-    });
-};
-
-const newMessage = () => {
-    messages.push('New message');
-    reRender()
-}
-
-const Button = (props) => {
-    const handleClick = () => {
-        newMessage()
-    };
-
-    return <button style={{ color: '#ff0000', background:'green'}} onClick={handleClick}>{props.children}</button>;
-};
-
-const reRender = () => {
-    ReactDom.render(
-        <>
-            <MessageList messages={messages} />
-            <Button>Add</Button>
-        </>,
-        document.getElementById('app')
-    );
-};
-
-reRender();
+ReactDom.render(
+    <App />,
+    document.getElementById('app')
+);
