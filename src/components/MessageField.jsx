11048e46b107691d90325fc5e0a6390bf79bf0d0
Obukhov89 <75534740+Obukhov89@users.noreply.github.com>
2021-04-18 23:11:24 +0500

    Add files via upload

---
+++ src/components/MessageField.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Message from './Message.jsx';
+
+export default class MessageField extends React.Component {
+
+    state = {
+        messages: [
+            'droid: Привет'
+        ],
+    };
+
+    handleClick = () => {
+        this.setState((state) => ({ messages: [...this.state.messages, `user: ${input.value}`] }));
+
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevState.messages[prevState.messages.length - 1] !== `user: ${input.value}`) {
+            setTimeout(() => {
+                this.setState((state) => ({
+                    messages: [...this.state.messages, 'droid: Не приставай ко мне, я робот!']
+                }))
+            }, 1000);
+            setTimeout(() => { input.value = '' }, 1001)
+        }
+    }
+
+
+    render() {
+        const messageElements = this.state.messages.map((text, index) => (
+            <Message key={index} text={text} />));
+
+        return <div>
+            {messageElements}
+            <input type="text" id="input" />
+            <button onClick={this.handleClick}>Отправить сообщение</button>
+        </div>
+    }
+}
