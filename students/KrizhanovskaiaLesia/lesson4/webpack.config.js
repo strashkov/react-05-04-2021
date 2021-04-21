5e1588386974d5af2b24a61f2c9df46db46ea102
selyana <olesya.krizhanovskaya@yandex.ru>
2021-04-22 00:33:12 +0300

    added lesson4

---
+++ students/KrizhanovskaiaLesia/lesson4/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./index.jsx",
+  },
+  devtool: "source-map",
+  context: path.resolve(__dirname, "static_src"),
+  output: {
+    path: path.resolve(__dirname, "static", "build"),
+    filename: "app.js",
+  },
+  // devtool: "cheap-inline-module-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "static", "build"),
+    writeToDisk: true,
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "static_src"),
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/env", "@babel/preset-react"],
+          plugins: [
+            [
+              "@babel/plugin-proposal-class-properties",
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
