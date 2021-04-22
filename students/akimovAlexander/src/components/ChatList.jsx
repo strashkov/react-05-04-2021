a42780aa95f7f53372fe67e048980b32ffc482bc
Alexander <impoossible.777@gmail.com>
2021-04-22 17:07:58 +0400

    add page Profile

--- students/akimovAlexander/src/components/ChatList.jsx
+++ students/akimovAlexander/src/components/ChatList.jsx
@@ -4,35 +4,61 @@ import { Link } from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
 import '../styles/style.css';
 
-const chats = [
-    {
-        id: '1',
-        text: 'Chat 1'
-    },
-    {
-        id: '2',
-        text: 'Chat 2'
-    },
-    {
-        id: '3',
-        text: 'Chat 3'
-    }
-];
+
 
 export default class ChatList extends React.Component {
+    static propTypes = {
+        chatId: PropTypes.string,
+        chats: PropTypes.object.isRequired,
+        onAddChat: PropTypes.func.isRequired
+    };
+
+    state = {
+        chatName: ''
+    };
+
+    handleChatNameChange = (event) => {
+        this.setState({
+            chatName: event.target.value
+        });
+    };
+
+    handleAddChatClick = () => {
+        this.props.onAddChat(this.state.chatName);
+        this.setState({
+            chatName: ''
+        });
+    };
 
     render() {
+        const { chatName } = this.state;
+        const { chats } = this.props;
         return (
             <div className="chat-list">
                 <List>
-                    {chats.map(({ id, text }) => (
-                        <ListItem button key={id}>
-                            <div className="chat-list-icon" />
-                            <ListItemText primary={text} />
-                        </ListItem>
+                    {Object.entries(chats).map(([id, value]) => (
+                        <Link to={`/chat/${id}`} key={id}>
+                            <ListItem button selected={id === this.props.chatId}>
+                                <div className="chat-list-icon" />
+                                <ListItemText primary={value.title} />
+                            </ListItem>
+                        </Link>
                     ))}
+                    <ListItem button >
+                        <TextField
+                            value={chatName}
+                            onChange={this.handleChatNameChange} />
+                        <IconButton
+                            disabled={!chatName}
+                            onClick={this.handleAddChatClick}>
+                            <SendIcon />
+                        </IconButton>
+                    </ListItem>
                 </List>
             </div>
         );
