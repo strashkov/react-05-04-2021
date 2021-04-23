ea6a32ddbd86a72be1f7e8ca11f5e310b8f253ec
selyana <olesya.krizhanovskaya@yandex.ru>
2021-04-23 14:40:00 +0300

    5

---
+++ students/KrizhanovskaiaLesia/lesson5/static_src/components/Router.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Layout from "../components/Layout.jsx";
+import MessageField from "../containers/MessageField.js";
+import Profile from "../containers/Profile.js";
+import PropTypes from "prop-types";
+
+export default class Router extends React.Component {
+  static propTypes = {
+    chats: PropTypes.object.isRequired,
+  };
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/profile" />} />
+        <Route
+          path="/chat/:id"
+          render={(props) => {
+            const chatId = props.match.params.id;
+
+            return (
+              <Layout
+                title={`Messages: ${this.props.chats[chatId].title}`}
+                chatId={chatId}
+              >
+                <MessageField chatId={chatId} />
+              </Layout>
+            );
+          }}
+        />
+        <Route
+          path="/profile"
+          render={() => (
+            <Layout title="Profile Page">
+              <Profile />
+            </Layout>
+          )}
+        />
+      </Switch>
+    );
+  }
+}
