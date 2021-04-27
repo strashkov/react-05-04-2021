0238eee933e97a4862090bf0d397ec6a183221ea
selyana <olesya.krizhanovskaya@yandex.ru>
2021-04-28 00:14:50 +0300

    6

---
+++ students/KrizhanovskaiaLesia/lesson6/static_src/middlewares/messageMiddleware.js
@@ -0,0 +1,34 @@
+import { SEND_MESSAGE, sendMessage } from "../actions/messageActions";
+// import { GLOW_CHAT } from "../actions/chatActions";
+
+
+export default store => next => (action) => {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            if (action.sender === 'me') {
+                setTimeout(() => {
+                    const { chatId } = action;
+                    const state = store.getState();
+                    const { messages } = state.messageReducer;
+                    const { chats } = state.chatReducer;
+                    const { firstName } = state.profileReducer;
+                    const messageId = Object.keys(messages).length + 1;
+                    const sender = chats[chatId].title;
+
+                    const botAction = sendMessage({
+                        messageId,
+                        chatId,
+                        text: `Привет, ${firstName}! Я ${sender}.`,
+                        sender: 'bot'
+                    });
+
+                    store.dispatch(botAction);
+                }, 1000);
+            }
+
+ч
+            // case GLOW_CHAT:
+
+    }
+    return next(action);
+}
---
+++ students/KrizhanovskaiaLesia/lesson6/static_src/reducers/profileReducer.js
@@ -0,0 +1,8 @@
+const initialStore = {
+  firstName: "Rick",
+  lastName: "Deckard",
+};
+
+export default function profileReducer(store = initialStore) {
+  return store;
+}
