df31e46f31c8902ff5cd6c92aa223fee63a7e365
Aleksandr Strashkov <avstrashkov@avito.ru>
2021-04-28 20:16:46 +0300

    lesson 8

---
+++ lessons/8/src/components/MessageField/index.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Message from '../../containers/Message';
+import TextField from '@material-ui/core/TextField';
+import SendIcon from '@material-ui/icons/Send';
+import Fab from '@material-ui/core/Fab';
+import { CircularProgress } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import './style.css';
+
+export default class MessageField extends React.Component {
+    static propTypes = {
+        isLoading: PropTypes.bool,
+        chatId: PropTypes.string.isRequired,
+        messages: PropTypes.object.isRequired,
+        sendMessage: PropTypes.func.isRequired,
+        loadMessages: PropTypes.func.isRequired
+    };
+
+    state = {
+        input: ''
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.messageFieldRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const { chatId, loadMessages } = this.props;
+
+        loadMessages(chatId);
+    }
+
+    componentDidUpdate(prevProps) {
+        const { chatId, loadMessages } = this.props;
+
+        if (prevProps.chatId !== chatId) {
+            loadMessages(chatId);
+        }
+
+        this.messageFieldRef.current.scrollTop =
+            this.messageFieldRef.current.scrollHeight - this.messageFieldRef.current.clientHeight;
+    }
+
+    sendMessage = () => {
+        const { input } = this.state;
+
+        if (!input) {
+            return;
+        }
+
+        const { chatId, messages } = this.props;
+        const lastMessageId = Number(Object.keys(messages).pop());
+        const messageId = lastMessageId + 1;
+
+        this.props.sendMessage({
+            messageId,
+            chatId,
+            text: input,
+            sender: 'me'
+        });
+
+        this.setState({
+            input: ''
+        });
+    };
+
+    handleChangeInput = ({ target: { value } }) => {
+        this.setState({
+            input: value
+        })
+    };
+
+    handleInputKeyUp = (event) => {
+        if (event.keyCode === 13) {
+            this.sendMessage();
+        }
+    };
+
+    render() {
+        const { messages, chatId, isLoading } = this.props;
+
+        const messageElements = Object.entries(messages).map(([messageId, message]) => {
+            const { text, sender } = message;
+
+            return (
+                <Message
+                    key={messageId}
+                    chatId={chatId}
+                    messageId={messageId}
+                    text={text}
+                    sender={sender} />
+            )
+        });
+
+        return (
+            <>
+                <div ref={this.messageFieldRef} className="message-field">
+                    { isLoading ? <CircularProgress /> : messageElements }
+                </div>
+                <div className='actions'>
+                    <TextField
+                        style={{ marginRight: '12px' }}
+                        placeholder='Введите сообщение'
+                        fullWidth
+                        value={this.state.input}
+                        type="text"
+                        autoFocus
+                        onKeyUp={this.handleInputKeyUp}
+                        onChange={this.handleChangeInput} />
+                    <Fab
+                        color='primary'
+                        disabled={this.state.input === ''}
+                        onClick={this.sendMessage}>
+                        <SendIcon />
+                    </Fab>
+                </div>
+            </>
+        )
+    }
+}
--- lessons/8/src/components/Router.jsx
+++ lessons/8/src/components/Router.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Layout from '../Layout/Layout';
-import MessageField from '../../containers/MessageField';
-import Profile from '../../containers/Profile';
-
+import Layout from '../components/Layout';
+import MessageField from '../containers/MessageField';
+import Profile from '../containers/Profile';
+import { CHAT_PATTERN } from '../constants';
 
 export default class Router extends React.Component {
     static propTypes = {
@@ -14,19 +14,19 @@ export default class Router extends React.Component {
         return (
             <Switch>
                 <Route exact path='/' render={() => (
-                    <Redirect to='/profile' />
+                    <Redirect to='/profile'/>
                 )} />
-                <Route path='/chat/:id' render={(props) => {
+                <Route path={CHAT_PATTERN} render={(props) => {
                     const chatId = props.match.params.id;
 
                     return (
                         <Layout
-                            title={`Messages: ${this.props.chats[chatId].title}`}
+                            title={`Messages: ${this.props.chats[chatId]?.title || ''}`}
                             chatId={chatId}>
                             <MessageField chatId={chatId} />
                         </Layout>
                     );
-                }} />
+                }}/>
                 <Route path='/profile' render={() => (
                     <Layout title='Profile Page'>
                         <Profile />
