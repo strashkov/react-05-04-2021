2086efb1b90da923acebc3c1450dd34d08be241e
selyana <olesya.krizhanovskaya@yandex.ru>
2021-05-06 14:23:23 +0300

    8

---
+++ students/KrizhanovskaiaLesia/lesson8/static_src/containers/ChatList.js
@@ -0,0 +1,16 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { addChat } from '../actions/chatActions.js';
+import ChatList from '../components/ChatList.jsx';
+
+const mapStateToProps = (store) => {
+    return {
+        chats: store.chatReducer.chats,
+    };
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    addChat
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
---
+++ students/KrizhanovskaiaLesia/lesson8/static_src/reducers/profileReducer.js
@@ -0,0 +1,9 @@
+const initialStore = {
+  firstName: 'Rick',
+  lastName: 'Decard'
+};
+
+
+export default function profileReducer(store = initialStore) {
+  return store;
+}
