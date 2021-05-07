7c04863d2012b85160e06eb1d030457eecb71c69
Alexander Vitshas <avitshas@gmail.com>
2021-05-07 23:26:21 +0300

    Пока открыли доступ к д/з, загружаю заглушку

---
+++ students/VitshasAlexander/prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: "all",
+  jsxBracketSameLine: false,
+};
